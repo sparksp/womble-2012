d996ec494ede8ef4164e8b3f449f46a1d55e2d9f
Phill Sparks <me@phills.me.uk>
2012-03-07 15:41:50 +0000

    Updated watchr script
    Signed-off-by: Phill Sparks <me@phills.me.uk>

--- watch.rb
+++ watch.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env watchr
 
-# Watch for changes to any LESS files and recompile the phills.less file
-watch( 'public/lib/.*\.less' ) {|md|
+def compile_less
 	system("lessc public/lib/womble.less > public/css/womble.css")
 	system("lessc public/lib/womble.less > public/css/womble.min.css --compress")
 	puts Time.now.strftime("%H:%M:%S") + " Compiled womble.less"
+end
+
+
+
+# Watch for changes to any LESS files and recompile the phills.less file
+watch( 'public/lib/.*\.less' ) {|md|
+	compile_less
 }
+compile_less # Run this when we launch too
